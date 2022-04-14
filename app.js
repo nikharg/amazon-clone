6bbb263b077f3eaea02688e320996e9389634557
Nikhar Gupta <nikhar.gupta13@gmail.com>
2022-04-14 12:00:30 +0530

    Deployment

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const DefaultData = require("./defaultdata");
 const cors = require("cors");
 const router = require("./routes/router");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(router);
 
+__dirname = path.resolve();
+
 const port = process.env.PORT || 8005;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => {
